bacdccc814d039cdb5c1286005cf28395711982d
temp@31 <137296693+iamak111@users.noreply.github.com>
2024-03-18 03:54:59 +0530

    Add files via upload

--- models/categorieModel.js
+++ models/categorieModel.js
@@ -1,42 +1,43 @@
-const mongoose = require('mongoose');
-const slugify = require('slugify');
+const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const categorieSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'A categorie must contain a name'],
-            trim: true,
-            unique: true
-        },
-        slug: {
-            type: String
-        },
-        for: {
-            type: String,
-            enum: [process.env.CATEGORYA, process.env.CATEGORYB],
-            required: [true, 'Product must contain website']
-        },
-        bannerImage: {
-            type: String,
-            required: true,
-            default: 'sample.jpg'
-        },
-        ecmcId: { type: String, required: true, unique: true }
+  {
+    name: {
+      type: String,
+      required: [true, "A categorie must contain a name"],
+      trim: true,
+      unique: true,
     },
-    {
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true },
-        timestamps: true
-    }
+    slug: {
+      type: String,
+    },
+    for: {
+      type: String,
+      enum: [process.env.CATEGORYA, process.env.CATEGORYB],
+      required: [true, "Product must contain website"],
+    },
+    bannerImage: {
+      type: String,
+      required: true,
+      default: "sample.jpg",
+    },
+    ecmcId: { type: String, required: true, unique: true },
+    subCategories: [String],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true,
+  }
 );
 
-categorieSchema.pre('save', async function (next) {
-    this.slug = slugify(this.name, { lower: true });
+categorieSchema.pre("save", async function (next) {
+  this.slug = slugify(this.name, { lower: true });
 
-    next();
+  next();
 });
 
-const categorieModel = mongoose.model('categories', categorieSchema);
+const categorieModel = mongoose.model("categories", categorieSchema);
 
 module.exports = categorieModel;
